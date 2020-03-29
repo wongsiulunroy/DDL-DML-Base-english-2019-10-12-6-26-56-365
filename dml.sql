73b451e762146d7579ac97107fff7de3d66508a9
hithere <wongsiulunroy@gmail.com>
2020-03-29 19:39:19 +0800

    update

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+INSERT INTO students(id, name, age, sex) VALUES('001', 'zhangsan', '18', 'male');
 -- Revise record
-
+UPDATE students SET age=19 WHERE name='zhangsan';
 --  Delete record
-
+DELETE FROM students WHERE name='zhangsan';
 -- Search record
+SELECT * FROM students WHERE id='001';
